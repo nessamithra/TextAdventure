2a8789df27dfe04d421ae7409f79285befb4f92d
Luisa Luettgens <luisa.luettgens@alumni.fh-aachen.de>
2022-02-16 13:37:49 +0100

    character initialized

--- Text Adventure/Character.cs
+++ Text Adventure/Character.cs
@@ -1,6 +1,10 @@
-﻿namespace Text_Adventure;
+﻿using System;
+
+namespace Text_Adventure;
 
 public class Character
 {
-    
+    private int Gold { get; set; }
+    private int HP { get; set; }
+    private int Attack { get; set; }
 }
