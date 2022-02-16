32328161a19444bfb7fbe09746ae55c9a11e20a3
Janna Hallbauer <hallbauer@fh-aachen.de>
2022-02-16 15:39:36 +0100

    Add Character maxhp

--- Text Adventure/Character.cs
+++ Text Adventure/Character.cs
@@ -6,12 +6,14 @@ public class Character
 {
     public int Gold { get; set; }
     public int HP { get; set; }
+    public int MaxHp { get; set; }
     public int Attack { get; set; }
-    
+
     public Character()
     {
         Gold = 0;
-        HP = 20;
+        MaxHp = 20;
+        HP = MaxHp;
         Attack = 3;
     }
 }
