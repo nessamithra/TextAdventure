fde692a0f747e370a636f1d841f7400331306b47
Janna Hallbauer <hallbauer@fh-aachen.de>
2022-02-16 13:18:14 +0100

    Refactor Room
    Removed unnecessary attributes.
    Added new method to get button texts.

--- Text Adventure/ChestRoom.cs
+++ Text Adventure/ChestRoom.cs
@@ -2,6 +2,11 @@
 
 public class ChestRoom : Room
 {
+    public override string[] GetTexts()
+    {
+        throw new System.NotImplementedException();
+    }
+
     public override bool DoIt()
     {
         throw new System.NotImplementedException();
