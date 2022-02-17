2e988a158dd77cfb48f15d80b7cd5a67a63fb80e
Luisa Luettgens <luisa.luettgens@alumni.fh-aachen.de>
2022-02-17 15:00:24 +0100

    added text to form

--- Text Adventure/Form1.Designer.cs
+++ Text Adventure/Form1.Designer.cs
@@ -257,8 +257,8 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.xp_value);
@@ -283,6 +283,7 @@
             this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
             this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
+            this.Text = "Text Adventure";
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.background)).EndInit();
             this.ResumeLayout(false);
